17bcdb390a485b2ab243d0817c32b1dfa38a14e8
todorakisa <todor.domitrov2000@gmail.com>
2017-12-13 00:22:11 +0200

    domashno

---
+++ spec/RSA_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/RSA.rb'
+describe RSA do
+    ressult = RSA.new(123, 456, 789)
+    it "checks return n" do
+        expect(ressult.n).to eq(123)
+    end
+ 
+    it "checks return e" do
+        expect(ressult.e).to eq(456)
+    end
+
+    it "checks return d" do
+        expect(ressult.d).to eq(789)
+    end
+	message = "test"
+	generated_keys = ressult.new_key
+    ressult_with_generated_keys = RSA.new(generated_keys[0], generated_keys[1], generated_keys[2])	
+    
+end
